8f9979d30e09ebb2a564c1a3f333c962b197c5d9
vijay <vijay.vj7@gmail.com>
2024-02-08 14:08:24 +0530

    New class added

---
+++ src/test/java/vjacademy/tests/GitClass.java
@@ -0,0 +1,18 @@
+package vjacademy.tests;
+
+import org.testng.annotations.Test;
+
+public class GitClass {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+
+	@Test
+	public void m2()
+	{
+		System.out.println("m2");
+	}
+}
